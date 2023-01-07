e9870ae258d05d886be6919add0e1373b5880358
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-01-07 17:02:23 -0500

    test: :white_check_mark: deckcreate and deck command
    add basic test for deck command but leave out more important testing

---
+++ cmd/deck_test.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestDeckCmd(t *testing.T) {
+	cmd := DeckCmd()
+	assert := func(want, got interface{}) {
+		if want != got {
+			t.Errorf("want %v, got %v", want, got)
+		}
+	}
+	got := cmd.Use
+	want := "deck [commands] [flags] [args]"
+	assert(want, got)
+
+	got = cmd.Short
+	want = "Manage the cards in a deck"
+	assert(want, got)
+
+}
+
+
+func TestDeckCreateCmd(t *testing.T) {
+	cmd := DeckCreateCmd()
+	assert := func(want, got interface{}) {
+		if want != got {
+			t.Errorf("want %v, got %v", want, got)
+		}
+	}
+	got := cmd.Use
+	want := "create [flags] <deck_type>"
+	assert(want, got)
+
+	got = cmd.Short
+	want = "Create a new deck"
+	assert(want, got)
+
+	// if err := cmd.RunE(cmd, []string{"1"}); err != nil {
+	// 	t.Errorf("running the command returned an error: %v", err)
+	// }
+}
