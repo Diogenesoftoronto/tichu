a76f87db4e182e85e0e874a74449cf65a92a24cc
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-01-07 17:05:10 -0500

    feat: :sparkles: create multiple decks!
    the deck create command can now be used to create as many decks as you want

--- cmd/deck.go
+++ cmd/deck.go
@@ -58,22 +58,24 @@ func DeckCreateCmd() *cobra.Command {
 		
 		Note that this argument returns the deck id of the new deck.`,
 		Args: cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("deck create called")
-			deck_type, err := strconv.ParseInt(args[0], 10, 8)
-			if err != nil {
+		RunE: func(cmd *cobra.Command, args []string) (error) {
+			flag, err := cmd.Flags().GetInt("amount"); if err != nil {
 				return err
 			}
-
-			deck_int := int(deck_type)
-			if dtype, err := deck.ParseDeck_type(deck_int); err != nil {
+			deck_type, err := strconv.ParseInt(args[0], 10, 8); if err != nil {
 				return err
-			} else {
+			}
+			deck_int := int(deck_type)
+			dtype, err := deck.ParseType(deck_int); if err != nil {
+				return err
+			} 
+			for i := 0; i < flag; i++ {
 				d := deck.New(dtype)
 				message := fmt.Sprintf("Created deck %s", d.Id)
-				fmt.Println(message)
-				return nil
+				sender := cmd.OutOrStdout()
+				fmt.Fprintln(sender, message)
 			}
+			return nil
 		},
 	}
 	return deckCreateCmd
